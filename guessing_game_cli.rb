97a2490e30f0cbee81c7e0587a9c234719c9735f
John Reiner <johnreiner1990@gmail.com>
2020-01-18 18:57:55 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -5,9 +5,9 @@ def run_guessing_game
   user_numb = gets.chomp
   if user_numb == 'exit'
     puts "Goodbye!"
-  elsif user_numb != comp_numb
+  if user_numb != comp_numb
     puts "Sorry! The computer guessed #{comp_numb}."
-  elsif user_numb.to_i == comp_numb
+  if user_numb.to_i == comp_numb
     puts "You guessed the correct number!"
   end
 

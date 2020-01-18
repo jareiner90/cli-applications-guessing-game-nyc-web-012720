e855c66c57736334dd05891ca3f8073513c00a57
John Reiner <johnreiner1990@gmail.com>
2020-01-18 19:05:17 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -7,6 +7,10 @@ def run_guessing_game
   user_numb = gets.chomp
 
   if user_numb == 'exit'
-    puts 
+    puts "Goodbye!"
+  else
+    user_numb = user_numb.to_i
+    if user_numb == comp_numb
+      puts 
 
 end

7c544229c0dda73d7696c822ee3fd2f911f50b38
John Reiner <johnreiner1990@gmail.com>
2020-01-18 18:55:52 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,7 +1,7 @@
 # Code your solution here!
 def run_guessing_game
   puts "Please enter a number 1 - 6: "
-  comp_numb = rand(1..6)
+  comp_numb = 1+rand(6)
   user_numb = gets.chomp
   if user_numb == 'exit'
     puts "Goodbye!"
